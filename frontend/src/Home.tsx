035e6a0295cd5fcb75ad0025fd573231125ed98e
LocalPiper <367548@niuitmo.ru>
2024-01-15 03:21:54 +0300

    feat: added inputs and buttons

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,41 +1,87 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Header } from "./Index";
 import { Footer } from "./Index";
 
 function Clock() {
-    return (
-        <>
-        <h2>Clock</h2>
-        </>
-    )
+  return (
+    <>
+      <h2>Clock</h2>
+      TODO: add clock
+    </>
+  );
 }
 
 function InputGroup() {
-    return (
-        <>TODO: inputs</>
-    )
+  return (
+    <>
+      <div className="input-group flex-nowrap">
+        <span className="input-group-text" id="addon-wrapping">
+          Username
+        </span>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter your name"
+          aria-label="Username"
+          aria-describedby="addon-wrapping"
+        />
+      </div>
+      <div className="input-group flex-nowrap">
+        <span className="input-group-text" id="addon-wrapping">
+          Password
+        </span>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Enter your password"
+          aria-label="Password"
+          aria-describedby="addon-wrapping"
+        />
+      </div>
+    </>
+  );
 }
 
 function ButtonGroup() {
-    return (
-        <>TODO: buttons</>
-    )
+  const navigate = useNavigate();
+
+  function handleClick(_event: any) {
+    navigate("/main");
+  }
+  return (
+    <>
+      <div className="btn-group" role="group" aria-label="Basic example">
+        <button type="button" onClick={handleClick} className="btn btn-primary">
+          Sign In
+        </button>
+        <button type="button" onClick={handleClick} className="btn btn-primary">
+          Sign Up
+        </button>
+      </div>
+    </>
+  );
 }
 
 function ErrorGroup() {
-    return <>TODO: errors</>
+  return <>TODO: errors</>;
 }
 function AuthContainer() {
-    return (
-        <>
-        <h2>Log in/Register</h2>
-        <ul className="list-group">
-            <div className="col"><InputGroup /></div>
-            <div className="col"><ButtonGroup /></div>
-            <div className="col"><ErrorGroup /></div>
-        </ul>
-        </>
-    )
+  return (
+    <>
+      <h2>Log in/Register</h2>
+      <ul className="list-group">
+        <div className="col">
+          <InputGroup />
+        </div>
+        <div className="col">
+          <ButtonGroup />
+        </div>
+        <div className="col">
+          <ErrorGroup />
+        </div>
+      </ul>
+    </>
+  );
 }
 function Base() {
   return (
@@ -44,8 +90,12 @@ function Base() {
         <h1>Web Lab 4</h1>
         <h3>Welcome to my lab! Please, proceed with the authorization.</h3>
         <div className="row">
-          <div className="col"><Clock /></div>
-          <div className="col"><AuthContainer /></div>
+          <div className="col">
+            <Clock />
+          </div>
+          <div className="col">
+            <AuthContainer />
+          </div>
         </div>
       </div>
     </>
@@ -59,8 +109,6 @@ export default function Home() {
         <Base></Base>
         <Footer></Footer>
       </div>
-
-      <Link to="/main">Start</Link>
     </>
   );
 }
