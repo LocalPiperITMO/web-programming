bfd86a20cdba6a27a21227ae89956c7c9747ebdb
LocalPiper <367548@niuitmo.ru>
2024-01-19 20:20:49 +0300

    feat: auth database configured

--- backend/src/main/java/com/example/backend/entity/Auth.java
+++ backend/src/main/java/com/example/backend/entity/Auth.java
@@ -3,12 +3,14 @@ package com.example.backend.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name="users", schema="public")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
